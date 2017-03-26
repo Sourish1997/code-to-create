7d57c97b0a77a33a04a21c7a3ada899b4a6c2fc5
Utkarsh Bajpai <utkarsh.bajpai2015@vit.ac.in>
2017-03-26 21:45:01 +0530

    Modify About Us UI

---
+++ app/src/main/java/acm/event/codetocreate17/UI/Fragments/AboutFragment.java
@@ -0,0 +1,66 @@
+package acm.event.codetocreate17.UI.Fragments;
+
+/**
+ * Created by Utkarsh._.Bajpai on 24-Mar-17.
+ */
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import acm.event.codetocreate17.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import yalantis.com.sidemenu.interfaces.ScreenShotable;
+
+
+public class AboutFragment extends Fragment implements ScreenShotable {
+    @BindView(R.id.about_root_layout)
+    CoordinatorLayout aboutContainer;
+
+    private Bitmap bitmap;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void takeScreenShot() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Bitmap bitmap = Bitmap.createBitmap(aboutContainer.getWidth(),
+                        aboutContainer.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                aboutContainer.draw(canvas);
+                AboutFragment.this.bitmap = bitmap;
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+}
