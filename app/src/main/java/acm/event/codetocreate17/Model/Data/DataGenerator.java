57cc36ec399c2b36746323ddb4d6b75775327968
Shikher Srivastava <shikher707@gmail.com>
2017-03-31 04:54:53 +0530

    Create About Us UI

--- app/src/main/java/acm/event/codetocreate17/Model/Data/DataGenerator.java
+++ app/src/main/java/acm/event/codetocreate17/Model/Data/DataGenerator.java
@@ -2,10 +2,14 @@ package acm.event.codetocreate17.Model.Data;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import acm.event.codetocreate17.R;
+import acm.event.codetocreate17.Model.Holders.AbouUsHolder;
+import acm.event.codetocreate17.View.Fragments.AboutFragment;
 
 /**
  * Created by Sourish on 19-03-2017.
@@ -23,33 +27,45 @@ public class DataGenerator {
     }
     public String[] getQuestions()
     {
-        String Ques[] = {"When will the Registration start?","What is the Registration Fees?","What about food and beverages?",
-                "Who all can participate?", "What are the skills required?" , "Is it an individual or team-challenge?"};
+        String Ques[] = {"Who all can register?","What are the registration fees?","How many people can I have on my team?",
+                "What tracks does this hack include?", "How do I choose my track?" , "How do I qualify for the pitching round?", "What is the quiz about ?"
+                , "What happens if I rank last during the quiz?", "What all can I win?", "What if I get hungry or need internet ? "};
         return Ques;
 
 
     }
     public String[] getAnswers()
     {
-        String Ans[] = {"We will open the registrations very soon! Keep following us.","Its free. All are welcome.",
-                "Its all on us! We would provide you with everything you need. We need you to concentrate on the code.",
-                "Students irrespective of their college/branch can take part in this event."
-                ,"Even if you don't know coding we will be having technical sessions before the event and mentors during the event."
-                ,"Its a team Challenge. It Should have a minimum of 2 and a maximum of 4 members."};
+        String Ans[] = {"Any and all students from all over the country can register. We welcome you all!",
+                "Nothing, it’s absolutely free!",
+                "You can have 2 to 4 people per team. You should either create your own team or accept invitations from other team admins.",
+                "Code2Create incorporates four main areas of interest: \n" +
+                        "-FinTech \n" +
+                        "-AR/VR \n" +
+                        "-Clean Energy \n" +
+                        "-Health Care","When you register we will take your preferences for each track and allot your track on a first-come-first-served basis. You will also select a problem statement from a given set."
+                ,"There will be two inspections conducted during the hack. After the first inspection, we will put the bottom 25 teams on the red list. If these teams do not show any improvement by the second inspection, they will be eliminated. Around 35 teams will pitch their ideas.",
+                "The quiz is for determining the final pitching order. It is based on GK and your Googling skills.",
+                "Don’t worry! We will simply ask you to pitch your idea first. We hope you are fast with powerpoint."
+                ,"Cash prizes and internships for first three teams, T-Shirts, certificates for participants, Digital Ocean credits, and stickers.",
+                "We’ll provide you with delicious food at regular intervals (breakfast, lunch, dinner and snacks) along with beverages. Also, you will be provided with free access to our beloved internet facility, VOLSBB.",};
         return Ans;
     }
     public static String getTimelineEvent(int i)
     {
-        String Event[] = {"Hackathon Event 1","Hackathon Event 2","Hackathon Event 3","Hackathon Event 4","Hackathon Event 5",
-                "Hackathon Event 6","Hackathon Event 7","Hackathon Event 8","Hackathon Event 9"};
+        String Event[] = {"Registration","Introduction","Hack begins","Dinner","Beverage period",
+                "Breakfast break","Fun activity","Lunch break","Technical inspection - 1","Red List Announcement",
+                "Snacks break","Fun activity","Dinner break","Sponsor presentation","Quiz",
+        "Technical inspection - 2","Final pitch list","Final pitch","Hack ends"};
         return Event[i];
 
     }
     public static String getTimelineDate(int i)
     {
-        String Date[] = {"2017-03-28 9:00","2017-03-28 18:00","2017-03-28 23:00","2017-03-29 00:46",
-                "2017-04-14 9:00","2017-04-14 9:00","2017-04-14 9:00","2017-04-14 9:00",
-                "2017-04-14 9:00","2017-04-14 9:00"};
+        String Date[] = {"2017-04-13 19:00","2017-04-13 21:00","2017-04-13 22:00","2017-04-13 23:30","2017-04-14 00:00",
+                "2017-04-14 9:00","2017-04-14 11:30","2017-04-14 13:00","2017-04-14 15:00",
+                "2017-04-14 17:00","2017-04-14 18:00","2017-04-14 19:30","2017-04-14 21:00","2017-04-14 22:00","2017-04-14 23:30"
+        ,"2017-04-15 2:00","2017-04-15 6:00","2017-04-15 8:30","2017-04-15 12:00","2017-04-15 18:00"};
         return Date[i];
 
     }
@@ -106,6 +122,128 @@ public class DataGenerator {
         VERTICAL
 
     }
+
+    public static AboutFragment.Group getFacultyOrganaiser(AboutFragment aboutFragment)
+    {
+        List<AbouUsHolder> childList=new ArrayList<>();
+        AbouUsHolder child1=new AbouUsHolder();
+        child1.setName("Dr. Aswani Kumar Cherukuri");
+        child1.setDesignation("Dean\n" +
+                "School of Information Technology & Engineering");
+        child1.setImageResource(R.drawable.dean_site);
+        AbouUsHolder child2=new AbouUsHolder();
+        child2.setName("Prof. H.R. Vishwakarma");
+        child2.setDesignation("Faculty Sponsor\n" +
+                "ACM VIT Student Chapter");
+        child2.setImageResource(R.drawable.hrv);
+        childList.add(child1);
+        childList.add(child2);
+        AboutFragment.Group group= aboutFragment.new Group(childList);
+        group.setName("Faculty Organisers");
+        return group;
+    }
+
+    public static AboutFragment.Group getStudentOrganiser(AboutFragment aboutFragment){
+
+        List<AbouUsHolder> childList=new ArrayList<>();
+        AbouUsHolder child1=new AbouUsHolder();
+        child1.setName("Abhinav Das");
+        child1.setDesignation("Presidnet");
+        child1.setImageResource(R.drawable.abhinav);
+        AbouUsHolder child2=new AbouUsHolder();
+        child2.setName("Pranay Gupta");
+        child2.setDesignation("Vice President");
+        child2.setImageResource(R.drawable.pranay);
+        AbouUsHolder child3=new AbouUsHolder();
+        child3.setName("Ashwini Purohit");
+        child3.setDesignation("Technical Head");
+        child3.setImageResource(R.drawable.ashwini);
+        AbouUsHolder child4=new AbouUsHolder();
+        child4.setName("Rahul Nigam");
+        child4.setDesignation("Design Head");
+        child4.setImageResource(R.drawable.rahul);
+        AbouUsHolder child5=new AbouUsHolder();
+        child5.setName("Rishi Raj");
+        child5.setDesignation("Deputy Technical Head");
+        child5.setImageResource(R.drawable.rishi);
+        AbouUsHolder child6=new AbouUsHolder();
+        child6.setName("Mugdha Pandaya");
+        child6.setDesignation("General Secretary");
+        child6.setImageResource(R.drawable.mugdha);
+        AbouUsHolder child7=new AbouUsHolder();
+        child7.setName("Tanish Noah");
+        child7.setDesignation("Treasurer");
+        child7.setImageResource(R.drawable.tanish);
+        AbouUsHolder child8=new AbouUsHolder();
+        child8.setName("Aarti Susan Kuruvilla");
+        child8.setDesignation("Communication Head");
+        child8.setImageResource(R.drawable.aarti);
+        AbouUsHolder child9=new AbouUsHolder();
+        child9.setName("Mallika Rai");
+        child9.setDesignation("University Relations");
+        child9.setImageResource(R.drawable.mallika);
+        AbouUsHolder child10=new AbouUsHolder();
+        child10.setName("Lekhani Ray");
+        child10.setDesignation("Membership Coordinator");
+        child10.setImageResource(R.drawable.lekhani);
+        childList.add(child1);
+        childList.add(child2);
+        childList.add(child3);
+        childList.add(child4);
+        childList.add(child5);
+        childList.add(child6);
+        childList.add(child7);
+        childList.add(child8);
+        childList.add(child9);
+        childList.add(child10);
+        AboutFragment.Group group= aboutFragment.new Group(childList);
+        group.setName("Student Organisers");
+        return group;
+    }
+
+    public static AboutFragment.Group getContacts(AboutFragment aboutFragment)
+    {
+
+        List<AbouUsHolder> childList=new ArrayList<>();
+        AbouUsHolder child1=new AbouUsHolder();
+        child1.setName("Facebook");
+        child1.setContact(true);
+        child1.setImageResource(R.drawable.ic_facebook);
+        child1.setDesignation("http://facebook.com/acm.vitu");
+
+        AbouUsHolder child2=new AbouUsHolder();
+        child2.setName("Website");
+        child2.setContact(true);
+        child2.setImageResource(R.drawable.ic_website);
+        child2.setDesignation("http://c2c.acmvit.in");
+
+
+
+        AbouUsHolder child3=new AbouUsHolder();
+        child3.setName("Mail");
+        child3.setContact(true);
+        child3.setImageResource(R.drawable.ic_email);
+        child3.setDesignation("mailto:outreach@acmvit.in");
+
+        AbouUsHolder child4=new AbouUsHolder();
+        child4.setName("CALL");
+        child4.setContact(true);
+        child4.setImageResource(R.drawable.ic_call);
+        child4.setDesignation("tel:919585386705");
+
+
+        childList.add(child1);
+        childList.add(child2);
+        childList.add(child3);
+        childList.add(child4);
+
+
+
+        AboutFragment.Group group= aboutFragment.new Group(childList);
+        group.setName("Contact Us");
+        return group;
+    }
+
 
 
 
---
+++ app/src/main/java/acm/event/codetocreate17/Utility/Adapters/AboutUsRecyclerAdapter.java
@@ -0,0 +1,136 @@
+package acm.event.codetocreate17.Utility.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bignerdranch.expandablerecyclerview.Adapter.ExpandableRecyclerAdapter;
+import com.bignerdranch.expandablerecyclerview.Model.ParentListItem;
+import com.bignerdranch.expandablerecyclerview.ViewHolder.ChildViewHolder;
+import com.bignerdranch.expandablerecyclerview.ViewHolder.ParentViewHolder;
+
+import java.util.List;
+
+import acm.event.codetocreate17.R;
+import acm.event.codetocreate17.Model.Holders.AbouUsHolder;
+import acm.event.codetocreate17.View.Fragments.AboutFragment;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static acm.event.codetocreate17.R.id.view;
+
+
+/**
+ * Created by Shikher on 31-03-2017.
+ */
+
+public class AboutUsRecyclerAdapter  {
+    public static class MyAdapter extends ExpandableRecyclerAdapter<MyAdapter.GroupViewHolder, MyAdapter.ItemViewHolder> {
+
+        private LayoutInflater mInflator;
+
+        public MyAdapter(Context context, @NonNull List<AboutFragment.Group> parentItemList) {
+            super(parentItemList);
+            mInflator = LayoutInflater.from(context);
+        }
+
+        @Override
+        public MyAdapter.GroupViewHolder onCreateParentViewHolder(ViewGroup parentViewGroup) {
+            View groupView = mInflator.inflate(R.layout.fragment_about_group, parentViewGroup, false);
+            return new MyAdapter.GroupViewHolder(groupView);
+        }
+
+        @Override
+        public MyAdapter.ItemViewHolder onCreateChildViewHolder(ViewGroup childViewGroup) {
+            View itemView = mInflator.inflate(R.layout.fragment_about_group_item, childViewGroup, false);
+            return new MyAdapter.ItemViewHolder(itemView);
+        }
+
+
+        @Override
+        public void onBindParentViewHolder(MyAdapter.GroupViewHolder parentViewHolder, int position, ParentListItem parentListItem) {
+            AboutFragment.Group group = (AboutFragment.Group) parentListItem;
+            parentViewHolder.bind(group);
+        }
+
+        @Override
+        public void onBindChildViewHolder(MyAdapter.ItemViewHolder childViewHolder, int position, Object childListItem) {
+            childViewHolder.bind(childListItem);
+        }
+
+        public class GroupViewHolder extends ParentViewHolder {
+
+            private TextView mGroupTextView;
+
+            public GroupViewHolder(View itemView) {
+                super(itemView);
+                mGroupTextView = (TextView) itemView.findViewById(R.id.about_list_item_content);
+            }
+
+            public void bind(AboutFragment.Group group) {
+                mGroupTextView.setText(group.getName());
+            }
+        }
+
+        public class ItemViewHolder extends ChildViewHolder {
+
+            private TextView mItemTextView, desgination;
+            private CircleImageView mImageView;
+            private View holder;
+
+            public ItemViewHolder(View itemView) {
+                super(itemView);
+                holder = itemView;
+                mImageView = (CircleImageView) itemView.findViewById(R.id.photo);
+                mItemTextView = (TextView) itemView.findViewById(R.id.name);
+                desgination = (TextView) itemView.findViewById(R.id.designation);
+            }
+
+            public void bind(Object item) {
+                final AbouUsHolder child = (AbouUsHolder) item;
+                if (child.getContact() == false) {
+                mItemTextView.setText(child.getName());
+                desgination.setText(child.getDesignation());
+                mImageView.setImageResource(child.getImageResource());
+
+            }
+            else
+                {
+                    mItemTextView.setText(child.getName());
+                    desgination.setVisibility(View.GONE);
+                    mImageView.setImageResource(child.getImageResource());
+                    holder.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (getAdapterPosition() == 3) {
+                                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(child.getDesignation()));
+                                if (intent.resolveActivity(v.getContext().getPackageManager()) != null) {
+                                    v.getContext().startActivity(intent);
+                                }
+                            } else if(getAdapterPosition()==2){
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(child.getDesignation()));
+                                if (intent.resolveActivity(v.getContext().getPackageManager()) != null) {
+
+                                    v.getContext().startActivity(intent);
+                                }
+                            } else{
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(child.getDesignation()));
+                                if (intent.resolveActivity(v.getContext().getPackageManager()) != null) {
+                                    v.getContext().startActivity(intent);
+                                }
+
+                            }
+                        }
+                    });
+                }
+
+                }
+        }
+    }
+}
